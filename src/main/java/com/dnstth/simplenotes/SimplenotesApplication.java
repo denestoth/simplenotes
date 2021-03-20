f443bc0ee0802d91db25dfd832f0da349699aa01
Denes Toth <denes.toth@outlook.com>
2021-03-20 17:11:04 +0100

    add generated project files

---
+++ src/main/java/com/dnstth/simplenotes/SimplenotesApplication.java
@@ -0,0 +1,13 @@
+package com.dnstth.simplenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplenotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplenotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnstth/simplenotes/SimplenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnstth.simplenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
